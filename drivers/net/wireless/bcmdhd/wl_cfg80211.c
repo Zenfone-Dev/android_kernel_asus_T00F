kitakar5525/linux-kernel

bcmdhd: initiate delayed work pm_enable_work in wl_cfg80211_attach
kernel panic in WIFI dhd driver when running stress testing.
Basically, pm_enable_work is scheduled, but __wl_cfg80211_up
jumps in and INIT_DELAYED_WORK the work again. Such operation
causes timer list on current cpu corrupted.

The patch fixes it by moving the initialiation to wl_cfg80211_attach.

Change-Id: Ia3eebf04aea130f31a753ac001847ac3fb5f23f8
Orig-Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-46532
Tracked-On: https://jira.ndg.intel.com/browse/MARVIN-1045
Signed-off-by: xiao jin <jin.xiao@intel.com>
Signed-off-by: chuangx <chengcaix.huang@intel.com>
Reviewed-on: https://android.intel.com:443/438187
Signed-off-by: Honore Tricot <honorex.tricot@intel.com>
Reviewed-on: https://android.intel.com/446166
Reviewed-by: Jarzmik, Robert <robert.jarzmik@intel.com>
Reviewed-by: Ranquet, GuillaumeX <guillaumex.ranquet@intel.com>
Reviewed-by: Maalem, Saadi <saadi.maalem@intel.com>
Reviewed-by: Tasayco Loarte, VictorX <victorx.tasayco.loarte@intel.com>
Tested-by: Tricot, Honore <honore.tricot@intel.com>
Reviewed-by: Tricot, Honore <honore.tricot@intel.com>

drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -11528,6 +11528,8 @@ s32 wl_cfg80211_attach(struct net_device *ndev, void *context)
 		goto cfg80211_attach_out;
 #endif 
 
+	INIT_DELAYED_WORK(&cfg->pm_enable_work, wl_cfg80211_work_handler);
+
 	g_bcm_cfg = cfg;
 
 #if defined(WL_ENABLE_P2P_IF)
@@ -12466,7 +12468,6 @@ static s32 __wl_cfg80211_up(struct bcm_cfg80211 *cfg)
 
 	err = dhd_monitor_init(cfg->pub);
 
-	INIT_DELAYED_WORK(&cfg->pm_enable_work, wl_cfg80211_work_handler);
 	wl_set_drv_status(cfg, READY, ndev);
 	return err;
 }

Zenfone-Dev/android_kernel_asus_T00F

Conflicts:
	drivers/net/wireless/bcmdhd/wl_cfg80211.c

drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -9956,6 +9956,8 @@ s32 wl_cfg80211_attach(struct net_device *ndev, void *data)
 		goto cfg80211_attach_out;
 #endif 
 
+	INIT_DELAYED_WORK(&wl->pm_enable_work, wl_cfg80211_work_handler);
+
 	wlcfg_drv_priv = wl;
 
 #if defined(WL_ENABLE_P2P_IF)
@@ -10660,7 +10662,6 @@ static s32 __wl_cfg80211_up(struct wl_priv *wl)
 	wl->scan_supp_timer.function = wl_cfg80211_scan_supp_timerfunc;
 	INIT_WORK(&wl->wlan_work, wl_cfg80211_work_handler);
 #endif /* DHCP_SCAN_SUPPRESS */
-	INIT_DELAYED_WORK(&wl->pm_enable_work, wl_cfg80211_work_handler);
 	wl_set_drv_status(wl, READY, ndev);
 	return err;
 }